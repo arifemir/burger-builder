432d5b029fc8beaf589e9210df95eb5b2eaa68d2
aearslanturk <arifarslanturk@gmail.com>
2020-06-18 04:03:39 +0300

    css repair and mediaqueries;

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 import BurgerIngrediend from "./BurgerIngrediend/BurgerIngrediend";
-import Fragment from "../../hoc/Fragment";
+import styles from './Burger.module.css'
 
 const Burger = (props) => {
   return (
-    <Fragment>
+    <div className={styles.Burger}>
       <BurgerIngrediend type='bread-top'/>
       <BurgerIngrediend type='salad'/>
       <BurgerIngrediend type='salad'/>
@@ -15,7 +15,7 @@ const Burger = (props) => {
       <BurgerIngrediend type='meat'/>
       <BurgerIngrediend type='meat'/>
       <BurgerIngrediend type='bread-bottom'/>
-    </Fragment>
+    </div>
   )
 }
 
