6edd1fe3d6bcd6612d18dc3340cde60702d69325
aearslanturk <arifarslanturk@gmail.com>
2020-06-21 01:46:09 +0300

    OrderSummary Modal styles upgraded;

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 import Fragment from "../../hoc/Fragment";
 import styles from './OrderSummary.module.css'
+import cn from 'classnames'
+import ControlLabel from "../BuildControlPanel/Control/ControlLabel/ControlLabel";
 const OrderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map(ingredientKey =>
-    <li key={ingredientKey}>
-      <span className={styles.Key}>
-        {ingredientKey}
-      </span>:
-      {props.ingredients[ingredientKey]}
-    </li>
+  const ingredientSummary = Object.keys(props.ingredients).map(ingredientKey => {
+    return  <li className={cn(styles.ListItem)} key={ingredientKey}>
+              <span className={styles.Key}>
+                <ControlLabel label={ingredientKey}/> : {props.ingredients[ingredientKey]}
+              </span>
+            </li>
+  }
   )
   return (
     <Fragment>
-      <h3>Your Order</h3>
-      <ul>
+      <h3 className={styles.Header}>Your Order</h3>
+      <ol className={styles.List}>
         {ingredientSummary}
-      </ul>
-      <p>Continue to checkout</p>
+      </ol>
+      <button className={styles.ContinueBtn}>Continue to checkout</button>
     </Fragment>
   );
 };
