7d2e50fed9e5c31e34c5911a5cddfee41a7bdda9
arifemir <arifarslanturk@gmail.com>
2020-09-08 05:57:56 +0300

    router added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Fragment from "./hoc/Fragment";
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
-
+import Checkout from "./containers/Checkout/Checkout"
+import {Route, Switch} from "react-router-dom"
 
 class App extends React.Component{
   render() {
     return (
       <Fragment>
         <Layout>
-          <BurgerBuilder/>
+          <Switch>
+          <Route path='/checkout' component={Checkout}/>
+          <Route path='/' exact component={BurgerBuilder} />
+          </Switch>
         </Layout>
       </Fragment>
     );
