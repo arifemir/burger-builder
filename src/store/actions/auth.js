74fae45d82b9f4b83e2372be9c6fa2763e79a3dc
Arif Emir Arslanturk <arifarslanturk@gmail.com>
2020-09-29 03:23:30 +0300

    -get the token from backend;

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,7 +1,7 @@
 import {AUTH_SUCCESS, AUTH_FAIL, AUTH_START} from './actionTypes'
+import axios from 'axios'
 
 export const authStart = () => {
-  console.log('auth start')
   return {
     type: AUTH_START
   }
@@ -10,7 +10,7 @@ export const authStart = () => {
 export const authSuccess = (authData) => {
   return {
     type: AUTH_SUCCESS,
-    authData: authData
+    authData
   }
 }
 
@@ -24,6 +24,21 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart())
-
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    }
+    const url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBkCQZa0wa4QZAP1xEvL2vsao9Mk-Wh1oA'
+    axios
+      .post(url, authData)
+      .then(res => {
+        console.log(res)
+        dispatch(authSuccess(res.data))
+      })
+      .catch(err => {
+        console.log(err)
+        dispatch(authFail(err.message))
+      })
   }
 }
